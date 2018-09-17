754cfbc5acc9d04fcd5471417a3b95867c0369eb
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-17 08:22:52 +0300

    generate password hash in users

--- app/models.py
+++ app/models.py
@@ -31,7 +31,7 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     blog = db.relationship('Blog', backref='user', lazy='dynamic')
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
 
@@ -68,8 +68,8 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer,primary_key = True)
     comment= db.Column(db.String(255))
-    blog_id = db.Column(db.Integer,db.ForeignKey('blog.id'))
-    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     username =  db.Column(db.String)
     posted = db.Column(db.DateTime,default=datetime.utcnow)
 
